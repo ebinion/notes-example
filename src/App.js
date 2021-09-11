e6644d6dfbfa5cb89c830d38a48f51d19c49463c
Zeke Binion <zeke@binion.io>
2021-09-10 23:15:40 -0400

    feat: layout app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <div className="App">App loaded</div>
+import { useState } from 'react'
+
+import IconedButton from './IconedButton'
+import Toolbar from './Toolbar'
+import { ReactComponent as PlusIcon } from './icons/plus-solid.svg'
+
+import styles from './App.module.css'
+
+const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const renderSidebar = () => {
+    return (
+      <div className={`${styles.nav} ${isMenuOpen && styles.navIsOpen}`}>
+        <div className={styles.navToolbar}>
+          <Toolbar trailingChildren={<IconedButton icon={<PlusIcon />} />} />
+        </div>
+        <div className={styles.navContent}>
+          <h1>Notes</h1>
+          <p>Sidebar</p>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {renderSidebar()}
+      <div className={styles.content}>Content</div>
+    </div>
+  )
 }
 
 export default App
