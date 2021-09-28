10ccd214be9f4f4fc608db7e94752b22f4ad168d
Zeke Binion <zeke@binion.io>
2021-09-28 09:02:53 -0400

    refactor: arrange views and scenes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,12 @@
-// import { useState } from 'react'
+import React from 'react' // eslint-disable-line no-unused-vars
+import PropTypes from 'prop-types'
 
-import IconedButton from './IconedButton'
-import Toolbar from './Toolbar'
-import { ReactComponent as PlusIcon } from './icons/plus-solid.svg'
-
-import { AppContext } from './AppContext'
-import List from './List'
-import Note from './Note'
-import Teaser from './Teaser'
-import styles from './App.module.css'
-
-const App = () => {
-  return (
-    <AppContext.Consumer>
-      {({
-        currentNoteID,
-        handleSetCurrentNote,
-        handleNewNote,
-        isNavOpen,
-        selectNotes,
-      }) => (
-        <main className={styles.wrapper}>
-          <nav className={`${styles.nav} ${isNavOpen && styles.navIsOpen}`}>
-            <div className={styles.navToolbar}>
-              <Toolbar
-                trailingChildren={
-                  <IconedButton icon={<PlusIcon />} onClick={handleNewNote} />
-                }
-              />
-            </div>
-
-            <header role="banner" className={styles.navHeader}>
-              <h1>Notes</h1>
-            </header>
-            <div className={styles.navContent}>
-              <List>
-                {selectNotes().map(note => {
-                  return (
-                    <Teaser
-                      isActive={note.id === currentNoteID}
-                      title={note.title}
-                      date={note.lastModifiedDate}
-                      onClick={event => handleSetCurrentNote(note, event)}
-                      key={note.id}
-                    />
-                  )
-                })}
-              </List>
-            </div>
-          </nav>
-          <section className={styles.content}>
-            <Note />
-          </section>
-        </main>
-      )}
-    </AppContext.Consumer>
-  )
+const App = props => {
+  return <div></div>
 }
+
+App.propTypes = {}
+
+App.defaultProps = {}
 
 export default App
---
+++ src/scenes/NotesScene.js
@@ -0,0 +1,61 @@
+// import { useState } from 'react'
+
+import IconedButton from '../views/IconedButton'
+import Toolbar from '../Toolbar'
+import { ReactComponent as PlusIcon } from './icons/plus-solid.svg'
+
+import { AppContext } from '../AppContext'
+import List from '../views/List'
+import Note from './Note'
+import Teaser from '../Teaser'
+import styles from './NotesScene.module.css'
+
+const NotesScene = () => {
+  return (
+    <AppContext.Consumer>
+      {({
+        currentNoteID,
+        handleSetCurrentNote,
+        handleNewNote,
+        isNavOpen,
+        selectNotes,
+      }) => (
+        <main className={styles.wrapper}>
+          <nav className={`${styles.nav} ${isNavOpen && styles.navIsOpen}`}>
+            <div className={styles.navToolbar}>
+              <Toolbar
+                trailingChildren={
+                  <IconedButton icon={<PlusIcon />} onClick={handleNewNote} />
+                }
+              />
+            </div>
+
+            <header role="banner" className={styles.navHeader}>
+              <h1>Notes</h1>
+            </header>
+            <div className={styles.navContent}>
+              <List>
+                {selectNotes().map(note => {
+                  return (
+                    <Teaser
+                      isActive={note.id === currentNoteID}
+                      title={note.title}
+                      date={note.lastModifiedDate}
+                      onClick={event => handleSetCurrentNote(note, event)}
+                      key={note.id}
+                    />
+                  )
+                })}
+              </List>
+            </div>
+          </nav>
+          <section className={styles.content}>
+            <Note />
+          </section>
+        </main>
+      )}
+    </AppContext.Consumer>
+  )
+}
+
+export default NotesScene
